c30870d21200fb08acf8d4405fbc3b76117cf414
Josh Moody <joshmoody24@gmail.com>
2025-05-22 03:24:40 -0600

    SQLite prevention fix

--- config/config.exs
+++ config/config.exs
@@ -51,3 +51,6 @@ import_config "aws.exs"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+# without this, migrations look at sqlite
+import_config "runtime.exs"
